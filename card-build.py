83803fccb53b67823ea3d898c19caedf5337d959
Melanie Wilson <melaniew1357@gmail.com>
2021-04-05 21:07:54 -0700

    changes to card.html

--- card-build.py
+++ card-build.py
@@ -1,9 +1,10 @@
+#grab templates
 top_template = open('top_template.html').read()
 bottom_template = open('bottom_template.html').read()
 
-#card
+#card content
 content = open('card.html').read()
 
-# Combine template HTML code with content HTML code
+# Combine template with content
 index_html = top_template + content + bottom_template
 open('index.html', 'w+').write(index_html)
